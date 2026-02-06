2d7a7c18d5f7de2cf0515e7db8ed46f19357430f
dps3007 <deepanshupal30072002@gmail.com>
2026-02-06 23:54:15 +0530

    5-02-26

---
+++ leetcode-daily/3379. Transformed Array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result;
+
+        for(int i = 0 ; i < n; i++){
+            result.push_back(nums[(( i + nums[i] ) % n + n ) % n]);
+        }
+        return result;
+    }
+};
